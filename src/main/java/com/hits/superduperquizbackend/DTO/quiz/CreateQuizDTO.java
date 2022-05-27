50e87cd994131d9616271146f9701fe5dc8dbb81
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-05-27 23:10:21 +0700

    Написан dto для создания викторины

---
+++ src/main/java/com/hits/superduperquizbackend/DTO/quiz/CreateQuizDTO.java
@@ -0,0 +1,16 @@
+package com.hits.superduperquizbackend.DTO.quiz;
+
+import com.hits.superduperquizbackend.entity.Difficult;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateQuizDTO {
+    private String name;
+    private String description;
+    private Difficult difficult;
+    private String categoryId;
+}
