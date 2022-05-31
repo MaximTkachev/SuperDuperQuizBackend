c1001e95e68b0c8e7a26a63ab50b4b79bde1cbbe
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-06-01 00:08:13 +0700

    Добавлены аннотации для валидации dto

--- src/main/java/com/hits/superduperquizbackend/DTO/quiz/CreateQuizDTO.java
+++ src/main/java/com/hits/superduperquizbackend/DTO/quiz/CreateQuizDTO.java
@@ -5,12 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateQuizDTO {
+
+    @NotBlank
     private String name;
+
     private String description;
+
+    @NotNull
     private Difficult difficult;
+
+    @NotBlank
     private String categoryId;
 }
