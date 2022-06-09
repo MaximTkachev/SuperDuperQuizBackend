2af968fe5d5e2fdc364c07d2cf0e3a22e2bd0dec
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-06-10 01:11:20 +0700

    Добавлен метод по поиску викторин по категории

--- src/main/java/com/hits/superduperquizbackend/repository/QuizRepository.java
+++ src/main/java/com/hits/superduperquizbackend/repository/QuizRepository.java
@@ -1,7 +1,12 @@
 package com.hits.superduperquizbackend.repository;
 
+import com.hits.superduperquizbackend.entity.CategoryEntity;
 import com.hits.superduperquizbackend.entity.QuizEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface QuizRepository extends JpaRepository<QuizEntity, String> {
+    List<QuizEntity> findByCategory(CategoryEntity category);
 }
