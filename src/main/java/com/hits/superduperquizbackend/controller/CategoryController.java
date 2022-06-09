3cdc15f5d5b981c13cb8820dc8d876748503ba29
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-06-10 00:30:13 +0700

    Реализован запрос на изменение категории
    Теперь через запрос возможно изменить название категории

--- src/main/java/com/hits/superduperquizbackend/controller/CategoryController.java
+++ src/main/java/com/hits/superduperquizbackend/controller/CategoryController.java
@@ -2,13 +2,13 @@ package com.hits.superduperquizbackend.controller;
 
 import com.hits.superduperquizbackend.DTO.category.CategoryDTO;
 import com.hits.superduperquizbackend.DTO.category.CreateCategoryDTO;
+import com.hits.superduperquizbackend.DTO.category.EditCategoryDto;
 import com.hits.superduperquizbackend.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/category")
@@ -20,5 +20,11 @@ public class CategoryController {
     @PostMapping
     public CategoryDTO createCategory(@Validated @RequestBody CreateCategoryDTO dto) {
         return categoryService.createCategory(dto);
+    }
+
+    @PatchMapping("/{id}")
+    public CategoryDTO editCategory(@Validated @RequestBody EditCategoryDto dto,
+                                    @PathVariable UUID id) {
+        return categoryService.editCategory(dto, id.toString());
     }
 }
--- src/main/java/com/hits/superduperquizbackend/service/CategoryService.java
+++ src/main/java/com/hits/superduperquizbackend/service/CategoryService.java
@@ -2,19 +2,31 @@ package com.hits.superduperquizbackend.service;
 
 import com.hits.superduperquizbackend.DTO.category.CategoryDTO;
 import com.hits.superduperquizbackend.DTO.category.CreateCategoryDTO;
+import com.hits.superduperquizbackend.DTO.category.EditCategoryDto;
 import com.hits.superduperquizbackend.entity.CategoryEntity;
+import com.hits.superduperquizbackend.exception.NotFoundException;
 import com.hits.superduperquizbackend.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
+    @Transactional
     public CategoryDTO createCategory(CreateCategoryDTO dto) {
         var entity = new CategoryEntity(dto);
         var savedEntity = categoryRepository.save(entity);
         return new CategoryDTO(savedEntity.getId(), savedEntity.getName());
+    }
+
+    @Transactional
+    public CategoryDTO editCategory(EditCategoryDto dto, String id) {
+        var entity = categoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Категория не найдена"));
+        entity.setName(dto.getName());
+        return new CategoryDTO(entity.getId(), entity.getName());
     }
 }
