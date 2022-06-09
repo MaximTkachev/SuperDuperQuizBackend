f9a6ddd677d62760aacda1fb6d4475c755ea6a85
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-06-10 00:40:21 +0700

    Написан конвертер для dto на викторины
    Конвертер пока имеет только один метод: конвертация сущности в dto

---
+++ src/main/java/com/hits/superduperquizbackend/converter/QuizConverter.java
@@ -0,0 +1,19 @@
+package com.hits.superduperquizbackend.converter;
+
+import com.hits.superduperquizbackend.DTO.quiz.CreateQuizDTO;
+import com.hits.superduperquizbackend.DTO.quiz.QuizDTO;
+import com.hits.superduperquizbackend.entity.QuizEntity;
+
+import java.util.HashSet;
+
+public class QuizConverter {
+    public static QuizDTO entityToDTO(QuizEntity entity) {
+        return new QuizDTO(
+                entity.getId(),
+                entity.getName(),
+                entity.getDescription(),
+                entity.getDifficult(),
+                entity.getCategory().getId(),
+                new HashSet<>());
+    }
+}
