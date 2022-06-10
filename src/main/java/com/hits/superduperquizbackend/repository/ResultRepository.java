6a1c18e1847b339629c614cc6fba162483058740
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-06-10 12:04:30 +0700

    Добавлен метод получения результатов по викторине

--- src/main/java/com/hits/superduperquizbackend/repository/ResultRepository.java
+++ src/main/java/com/hits/superduperquizbackend/repository/ResultRepository.java
@@ -1,7 +1,11 @@
 package com.hits.superduperquizbackend.repository;
 
+import com.hits.superduperquizbackend.entity.QuizEntity;
 import com.hits.superduperquizbackend.entity.ResultEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ResultRepository extends JpaRepository<ResultEntity, String> {
+    List<ResultEntity> findAllByQuiz(QuizEntity quiz);
 }
